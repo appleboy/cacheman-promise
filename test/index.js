50541b0747af4606fc635551272f3f3429a08059
Bo-Yi Wu <appleboy.tw@gmail.com>
2015-05-10 19:05:06 +0800

    add mongo and redis testing.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- test/index.js
+++ test/index.js
@@ -12,7 +12,7 @@ describe('Cache Engine test:', function() {
     done();
   });
 
-  after(function(done) {
+  afterEach(function(done) {
     cache.clear();
     done();
   });
@@ -23,6 +23,50 @@ describe('Cache Engine test:', function() {
     cache.del.should.be.ok;
     cache.clear.should.be.ok;
     cache.wrap.should.be.ok;
+  });
+
+  it('should store items', function (done) {
+    cache.set('test1', { a: 1 }, function (val) {
+
+      cache.get('test1', function (val) {
+
+        val.a.should.be.eql(1);
+        done();
+      });
+    });
+  });
+
+  it('should store zero', function (done) {
+    cache.set('test2', 0, function (val) {
+
+      cache.get('test2', function (val) {
+
+        val.should.be.eql(0);
+        done();
+      });
+    });
+  });
+
+  it('should store false', function (done) {
+    cache.set('test3', false, function (val) {
+
+      cache.get('test3', function (val) {
+
+        val.should.be.false;
+        done();
+      });
+    });
+  });
+
+  it('should store null', function (done) {
+    cache.set('test4', null, function (val) {
+
+      cache.get('test4', function (val) {
+
+        should.not.exist(val);
+        done();
+      });
+    });
   });
 
   it('get empty cache value if cache not found and no default value', function(done) {
@@ -197,5 +241,33 @@ describe('Cache Engine test:', function() {
 
         done();
       });
+  });
+
+  it('should accept `redis` as valid engine', function (done) {
+    var key = faker.name.findName();
+    var data = faker.name.findName();
+    cache = new Cacheman('testing', {engine: 'redis'});
+    cache.set(key, {name: data}, function (val) {
+
+      cache.get(key, function (val) {
+
+        val.name.should.be.eql(data);
+        done();
+      });
+    });
+  });
+
+  it('should accept `mongo` as valid engine', function (done) {
+    var key = faker.name.findName();
+    var data = faker.name.findName();
+    cache = new Cacheman('testing', {engine: 'mongo'});
+    cache.set(key, {name: data}, function (val) {
+
+      cache.get(key, function (val) {
+
+        val.name.should.be.eql(data);
+        done();
+      });
+    });
   });
 });
