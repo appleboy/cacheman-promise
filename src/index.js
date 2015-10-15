aa133562cb87a2fefa0c8c7bdc32844a0497c788
Bo-Yi Wu <appleboy.tw@gmail.com>
2015-10-15 15:54:39 +0800

    fix #8 support ES2015
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- src/index.js
+++ src/index.js
@@ -10,248 +10,252 @@ import Cacheman from 'cacheman';
  * @api public
  */
 
-module.exports = function(name, options) {
+export default class CachemanPromise {
 
-  if (name && _.isObject(name)) {
-    options = name;
-    name = null;
+  /**
+   * Class constructor method.
+   *
+   * @param {String} name
+   * @param {Object} [options]
+   * @return {Cacheman} this
+   * @api public
+   */
+
+  constructor(name, options = {}) {
+
+    if (name && _.isObject(name)) {
+      options = name;
+      name = null;
+    }
+
+    this.cache = new Cacheman(name, options);
   }
 
-  var cache = new Cacheman(name, options);
+  /**
+   * Wraps a function in cache. I.e., the first time the function is run,
+   * its results are stored in cache so subsequent calls retrieve from cache
+   * instead of calling the function.
+   *
+   * @param {String} key
+   * @param {Function || String} data
+   * @param {Number} ttl
+   * @param {Function} cb
+   *
+   * @return promise
+   * @api public
+   */
+  wrap = (key, data, ttl, cb) => {
+    let isCached = false;
 
-  return {
+    if (typeof ttl === 'function') {
+      cb = ttl;
+      ttl = null;
+    }
 
-    /**
-     * Wraps a function in cache. I.e., the first time the function is run,
-     * its results are stored in cache so subsequent calls retrieve from cache
-     * instead of calling the function.
-     *
-     * @param {String} key
-     * @param {Function || String} data
-     * @param {Number} ttl
-     * @param {Function} cb
-     *
-     * @return promise
-     * @api public
-     */
-    wrap: function(key, data, ttl, cb) {
-      var isCached = false;
-      var _this = this;
+    return new Promise((resolve, reject) => {
+      this.cache.get(key, (error, val) => {
 
-      if (typeof ttl === 'function') {
-        cb = ttl;
-        ttl = null;
-      }
-
-      return new Promise(function(resolve, reject) {
-        cache.get(key, function(error, val) {
-
-          if (error) {
-            return reject(error);
-          }
-
-          return resolve(val);
-        });
-      }).then(function(val) {
-
-        // return cache value.
-        if (!_.isUndefined(val) && !_.isNull(val)) {
-          isCached = true;
-          return val;
+        if (error) {
+          return reject(error);
         }
 
-        // return default value.
-        if (_.isFunction(data)) {
-          return data();
+        return resolve(val);
+      });
+    }).then((val) => {
+
+      // return cache value.
+      if (!_.isUndefined(val) && !_.isNull(val)) {
+        isCached = true;
+        return val;
+      }
+
+      // return default value.
+      if (_.isFunction(data)) {
+        return data();
+      }
+
+      return data || null;
+    }).then((val) => {
+
+      // set cache if missing cache.
+      if (!isCached) {
+        this.set(key, val, ttl);
+      }
+
+      return (cb && _.isFunction(cb)) ? cb(val) : val;
+    });
+  }
+
+  /**
+   * Get an entry.
+   *
+   * @param {String|Array} key
+   * @param {Function} cb
+   * @return promise
+   *
+   * @api private
+   */
+  _get = (key, cb) => {
+
+    return new Promise((resolve, reject) => {
+      this.cache.get(key, (error, val) => {
+
+        if (error) {
+          return reject(error);
         }
 
-        return data || null;
-      }).then(function(val) {
+        return resolve(val);
+      });
+    }).then((val) => {
 
-        // set cache if missing cache.
-        if (!isCached) {
-          _this.set(key, val, ttl);
+      return (cb && _.isFunction(cb)) ? cb(val) : val;
+    });
+  }
+
+  /**
+   * Get multiple or single entry.
+   *
+   * @param {String|Array} key
+   * @param {Function} cb
+   * @return promise
+   *
+   * @api public
+   */
+  get = (key, cb) => {
+
+    if (_.isArray(key)) {
+      let item = {};
+      _.forEach(key, (value) => {
+        item[value] = this._get(value);
+      });
+
+      return Promise.props(item);
+    }
+
+    return this._get(key, cb);
+  }
+
+  /**
+   * Set an entry.
+   *
+   * @param {String} key
+   * @param {Mixed} data
+   * @param {Number} ttl
+   * @param {Function} cb
+   *
+   * @return promise
+   * @api public
+   */
+  set = (key, data, ttl, cb) => {
+
+    if (typeof ttl === 'function') {
+      cb = ttl;
+      ttl = null;
+    }
+
+    return new Promise((resolve, reject) => {
+      this.cache.set(key, data, ttl, (error, val) => {
+        if (error) {
+          return reject(error);
         }
 
-        return (cb && _.isFunction(cb)) ? cb(val) : val;
+        return resolve(val);
       });
-    },
+    }).then((val) => {
 
-    /**
-     * Get an entry.
-     *
-     * @param {String|Array} key
-     * @param {Function} cb
-     * @return promise
-     *
-     * @api private
-     */
-    _get: function(key, cb) {
+      return (cb && _.isFunction(cb)) ? cb(val) : val;
+    });
+  }
 
-      return new Promise(function(resolve, reject) {
-        cache.get(key, function(error, val) {
+  /**
+   * Set an entry.
+   *
+   * @param {String} key
+   * @param {Mixed} data
+   * @param {Number} ttl
+   * @param {Function} cb
+   *
+   * @return promise
+   * @api public
+   */
+  pull = (key, data) => {
+    // check default value is undefined or Null
+    if (_.isUndefined(data)) {
+      data = null;
+    }
 
-          if (error) {
-            return reject(error);
-          }
+    return new Promise((resolve, reject) => {
+      this.cache.get(key, (error, val) => {
 
-          return resolve(val);
-        });
-      }).then(function(val) {
+        if (error) {
+          return reject(error);
+        }
 
-        return (cb && _.isFunction(cb)) ? cb(val) : val;
+        return resolve(val);
       });
-    },
+    }).then((val) => {
 
-    /**
-     * Get multiple or single entry.
-     *
-     * @param {String|Array} key
-     * @param {Function} cb
-     * @return promise
-     *
-     * @api public
-     */
-    get: function(key, cb) {
+      // delete cache key
+      this.del(key);
 
-      if (_.isArray(key)) {
-        var _this = this;
-        var item = {};
-        _.forEach(key, function(value) {
-          item[value] = _this._get(value);
-        });
+      return (!_.isUndefined(val) && !_.isNull(val)) ? val : data;
+    });
+  }
 
-        return Promise.props(item);
-      }
+  /**
+   * Delete an entry.
+   *
+   * @param {String|Array} key
+   * @param {Function} cb
+   *
+   * @return promise
+   * @api public
+   */
+  del = (key, cb) => {
 
-      return this._get(key, cb);
-    },
+    if (_.isString(key)) {
+      key = [key];
+    }
 
-    /**
-     * Set an entry.
-     *
-     * @param {String} key
-     * @param {Mixed} data
-     * @param {Number} ttl
-     * @param {Function} cb
-     *
-     * @return promise
-     * @api public
-     */
-    set: function(key, data, ttl, cb) {
+    return new Promise.resolve(key)
+      .map((row) => {
+        return new Promise((resolve, reject) => {
+          this.cache.del(row, (err, data) => {
 
-      if (typeof ttl === 'function') {
-        cb = ttl;
-        ttl = null;
-      }
+            if (err) {
+              return reject(err);
+            }
 
-      return new Promise(function(resolve, reject) {
-        cache.set(key, data, ttl, function(error, val) {
-
-          if (error) {
-            return reject(error);
-          }
-
-          return resolve(val);
-        });
-      }).then(function(val) {
-
-        return (cb && _.isFunction(cb)) ? cb(val) : val;
-      });
-    },
-
-    /**
-     * Set an entry.
-     *
-     * @param {String} key
-     * @param {Mixed} data
-     * @param {Number} ttl
-     * @param {Function} cb
-     *
-     * @return promise
-     * @api public
-     */
-    pull: function(key, data) {
-      var _this = this;
-
-      // check default value is undefined or Null
-      if (_.isUndefined(data)) {
-        data = null;
-      }
-
-      return new Promise(function(resolve, reject) {
-        cache.get(key, function(error, val) {
-
-          if (error) {
-            return reject(error);
-          }
-
-          return resolve(val);
-        });
-      }).then(function(val) {
-
-        // delete cache key
-        _this.del(key);
-
-        return (!_.isUndefined(val) && !_.isNull(val)) ? val : data;
-      });
-    },
-
-    /**
-     * Delete an entry.
-     *
-     * @param {String|Array} key
-     * @param {Function} cb
-     *
-     * @return promise
-     * @api public
-     */
-    del: function(key, cb) {
-
-      if (_.isString(key)) {
-        key = [key];
-      }
-
-      return new Promise.resolve(key)
-        .map(function(row) {
-          return new Promise(function(resolve, reject) {
-            cache.del(row, function(err, data) {
-
-              if (err) {
-                return reject(err);
-              }
-
-              return resolve();
-            });
+            return resolve(data);
           });
-        }).then(function() {
-          return (cb && _.isFunction(cb)) ? cb() : '';
-        });
-    },
-
-    /**
-     * Clear all entries.
-     *
-     * @param {Function} cb
-     *
-     * @return promise
-     * @api public
-     */
-    clear: function(cb) {
-
-      return new Promise(function(resolve, reject) {
-        cache.clear(function(error) {
-
-          if (error) {
-            return reject(error);
-          }
-
-          return resolve();
         });
       }).then(function() {
-
         return (cb && _.isFunction(cb)) ? cb() : '';
       });
-    }
-  };
-};
+  }
+
+  /**
+   * Clear all entries.
+   *
+   * @param {Function} cb
+   *
+   * @return promise
+   * @api public
+   */
+  clear = (cb) => {
+
+    return new Promise((resolve, reject) => {
+      this.cache.clear((error) => {
+
+        if (error) {
+          return reject(error);
+        }
+
+        return resolve();
+      });
+    }).then(function() {
+
+      return (cb && _.isFunction(cb)) ? cb() : '';
+    });
+  }
+}
